73c3a57a70c0c80504dc42917df2c09fb79d83bc
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-06-14 20:53:29 -0400

    API running, Docker running, not perfect but good enough

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,65 +1,69 @@
 from flask_restx import Namespace, Resource
-from app.services.review_service import ReviewService
+from app.services.user_service import UserService
 from app.persistence.data_manager import DataManager
+from email_validator import validate_email, EmailNotValidError
 
-api = Namespace('reviews', description='Review operations')
-review_service = ReviewService(DataManager())
+"""This user routes passed 9/9 tests"""
 
-@api.route('/places/<place_id>/reviews')
-@api.param('place_id', 'The place identifier')
-class ReviewByPlace(Resource):
-    @api.doc('create_review')
-    def post(self, place_id):
-        """Create a review for a place"""
+api = Namespace('users', description='User operations')
+user_service = UserService(DataManager())
+
+
+# This route was refactored to be more robust 6/13/2024
+@api.route('/')
+class UserList(Resource):
+    @api.doc('list_users')
+    def get(self):
+        """List all users"""
+        users = user_service.get_all_users()
+        return users, 200  # Directly return the list of dictionaries
+        # return [user.to_dict() for user in users], 200
+
+    @api.doc('create_user')
+    def post(self):
+        """Create a new user"""
         data = api.payload
+        required_fields = ['email', 'first_name', 'last_name', 'password']  # Include 'password' in the required fields
+        if not all(field in data for field in required_fields):
+            return {"error": "Missing required field"}, 400
+
         try:
-            review = review_service.create_review(place_id, data['user_id'], data['rating'], data['comment'])
-            return review.to_dict(), 201
+            # Now include 'password' when calling create_user
+            user = user_service.create_user(data['email'], data['first_name'], data['last_name'], data['password'])
+            return user.to_dict(), 201
+        except EmailNotValidError as e:
+            return {"error": str(e)}, 400
         except ValueError as e:
-            return {'error': str(e)}, 400
+            return {"error": str(e)}, 400
 
-    @api.doc('get_reviews_by_place')
-    def get(self, place_id):
-        """Get reviews by place ID"""
-        reviews = review_service.get_reviews_by_place(place_id)
-        return [review.to_dict() for review in reviews], 200
 
-@api.route('/users/<user_id>/reviews')
+@api.route('/<user_id>')
 @api.param('user_id', 'The user identifier')
-class ReviewByUser(Resource):
-    @api.doc('get_reviews_by_user')
+class User(Resource):
+    @api.doc('get_user')
     def get(self, user_id):
-        """Get reviews by user ID"""
-        reviews = review_service.get_reviews_by_user(user_id)
-        return [review.to_dict() for review in reviews], 200
-
-@api.route('/<review_id>')
-@api.param('review_id', 'The review identifier')
-class Review(Resource):
-    @api.doc('get_review')
-    def get(self, review_id):
-        """Get a single review by ID"""
+        """Fetch a user given its identifier"""
         try:
-            review = review_service.get_review(review_id)
-            return review.to_dict(), 200
+            user = user_service.get_user(user_id)
+            return user.to_dict(), 200
         except ValueError as e:
-            return {'error': str(e)}, 404
+            return {"error": str(e)}, 404
 
-    @api.doc('update_review')
-    def put(self, review_id):
-        """Update a review by ID"""
+    @api.doc('update_user')
+    def put(self, user_id):
+        """Update a user given its identifier"""
         data = api.payload
         try:
-            review = review_service.update_review(review_id, data['rating'], data['comment'])
-            return review.to_dict(), 200
+            user = user_service.update_user(user_id, **data)
+            return user.to_dict(), 200
         except ValueError as e:
-            return {'error': str(e)}, 400
+            return {"error": str(e)}, 404
 
-    @api.doc('delete_review')
-    def delete(self, review_id):
-        """Delete a review by ID"""
+    @api.doc('delete_user')
+    def delete(self, user_id):
+        """Delete a user given its identifier"""
         try:
-            review_service.delete_review(review_id)
+            user_service.delete_user(user_id)
             return '', 204
         except ValueError as e:
-            return {'error': str(e)}, 404
+            return {"error": str(e)}, 404
